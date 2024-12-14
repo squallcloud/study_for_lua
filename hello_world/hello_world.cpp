a27106e8079fe1cb7507a5556d14a30f3ec89bac
squallcloud <squallcloudzidane@gmail.com>
2024-12-15 01:35:34 +0900

    [update] lua を nuget から取得する

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
+#include <lua.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    lua_State* L = luaL_newstate();
+
+    luaL_openlibs(L);
+
+    luaL_dostring(L, "print('Hello, World!')");
+
+    lua_close(L);
+    return 0;
 }
