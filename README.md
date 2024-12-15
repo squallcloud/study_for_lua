7d1dc84d1372ed6bce543b1d69315caee0959452
squallcloud <squallcloudzidane@gmail.com>
2024-12-16 01:26:58 +0900

    Workbranch (#1)
    * [add] hello_world プロジェクト追加
    
    * [update] lua を nuget から取得する
    
    * [update] lua ファイルを実行する
    
    * [update] lua ファイルを実行する
    
    * [update] 出力先変更
    
    * [add] バイナリ作成を確認するためのプロジェクト追加
    
    * [update] バイナリ出力コード実装
    
    * [update] README に参考サイト追記

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ﻿# study_for_lua
+
+## 参考サイト
+- [Lua 5.4 リファレンスマニュアル](https://inzkyk.xyz/lua_5_4/#gsc.tab=0)
---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <lua.hpp>
+
+int main()
+{
+    lua_State* L = luaL_newstate();
+
+    luaL_openlibs(L);
+
+    luaL_dostring(L, "print('Hello, World!')");
+
+    luaL_dofile(L, "hello_world.lua");
+
+    lua_close(L);
+    return 0;
+}
